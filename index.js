d0e1b0e375aff2f80a6ce44fc65bcaecf4966f9b
Hasanul Bari <hasanulbari8@gmail.com>
2022-11-07 23:17:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wc7jl9l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+  async  function run() {
+      try {
+        const serviceCollection = client.db('greenTure').collection('services');
+
+        app.get('/services', async (req, res) => {
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services)
+        })
+        
+      }
+      finally {
+        
+      }
+      
+    }
+    run().catch((error) => {
+      console.log(error)
+    });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Green true server is running...')
+})
+
+app.listen(port, () => {
+    console.log(`Green ture server is running ${port}`)
+})
