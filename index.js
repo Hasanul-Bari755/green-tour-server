47c37a47a46162a48edd738dc17faa062b2125c6
Hasanul Bari <hasanulbari8@gmail.com>
2022-11-10 00:44:04 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,10 +13,35 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wc7jl9l.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ massage: "unauthorize" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_WEB_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ massage: "unauthorize" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
+
   async  function run() {
       try {
         const serviceCollection = client.db('greenTure').collection('services');
         const reviewCollecttion = client.db('greenTure').collection('review')
+
+        app.post("/jwt", (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_WEB_TOKEN, {
+        expiresIn: "1d",
+        });
+        res.send({ token });
+       });
 
         app.get('/services', async (req, res) => {
           const query = {}
@@ -60,7 +86,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
           }
           const cursor = reviewCollecttion.find(query);
           const review = await cursor.toArray()
-          const reviewsByTime = review.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+          const reviewsByTime = review.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).reverse()
           res.send(reviewsByTime)
 
         })
@@ -91,7 +117,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
           res.send(result)
         })
 
-        app.patch('/updatereview/:id', async (req, res) => {
+        app.patch('/updatereview/:id',verifyJWT, async (req, res) => {
           const id = req.params.id;
           const review = req.body;
           console.log(review)
